06331934f0c6c9a1cd959665c9fdd81735d89d43
Adham <asorour@sfu.ca>
2023-02-21 19:36:54 -0800

    only allow withdrawals below certain amount within a certain timewindow for each address, added tests

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,14 +1,48 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.17;
 
 contract Faucet {
+  mapping (address => uint) private lastWithdrawal;
+  uint public maxWithdrawal; // max withdrawal amount per transaction
+  uint public minWindow;     // min time between withdrawals for a given address
+  address private owner;
+
+  constructor(uint _maxWithdrawal, uint _minWindow) {
+    maxWithdrawal = _maxWithdrawal;
+    minWindow = _minWindow;
+    owner = msg.sender;
+  }
   
-  function withdraw(uint _amount) public {
-    // users can only withdraw .1 ETH at a time, feel free to change this!
-    require(_amount <= 100000000000000000);
-    payable(msg.sender).transfer(_amount);
+  error earlyWithdrawal(uint timeRemianing);
+
+  function withdraw(uint amount) external {
+    uint timeSinceLastWithdrawal = block.timestamp - lastWithdrawal[msg.sender];
+    if (timeSinceLastWithdrawal < minWindow) {
+      revert earlyWithdrawal(300 - timeSinceLastWithdrawal);
+    }
+
+    require(amount <= maxWithdrawal, "Withdrawal limit exceeded");
+
+    // contract must have sufficient funds
+    require(address(this).balance > amount, "Insufficient funds in faucet");
+
+    payable(msg.sender).transfer(amount);
+    lastWithdrawal[msg.sender] = block.timestamp;
   }
 
   // fallback function
   receive() external payable {}
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Not Owner");
+    _;
+  }
+
+  function setMaxWithdrawal(uint newLimit) external onlyOwner {
+    maxWithdrawal = newLimit;
+  }
+
+  function setMinWindow(uint newWindow) external onlyOwner {
+    minWindow = newWindow;
+  }
 }
---
+++ test/Faucet.js
@@ -0,0 +1,149 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const {
+	time,
+	loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+  
+
+describe("Faucet", function () {
+	async function deployContractFixture() {
+		const MAX_WITHDRAWAL = 1000;
+		const MIN_WINDOW = 24 * 60 * 60; // 24 hours in seconds
+
+		const factory = await ethers.getContractFactory("Faucet");
+		const faucet = await factory.deploy(MAX_WITHDRAWAL, MIN_WINDOW);
+		
+		// Contracts are deployed using the first signer/account by default
+		const [owner, otherAccount] = await ethers.getSigners();
+
+		return { faucet, MAX_WITHDRAWAL, MIN_WINDOW, owner, otherAccount };
+	}
+
+	async function deployContractWithFundsFixture() {
+		const MAX_WITHDRAWAL = 1000;
+		const MIN_WINDOW = 24 * 60 * 60; // 24 hours in seconds
+
+		const factory = await ethers.getContractFactory("Faucet");
+		const faucet = await factory.deploy(MAX_WITHDRAWAL, MIN_WINDOW);
+
+		// Contracts are deployed using the first signer/account by default
+		const [owner, otherAccount] = await ethers.getSigners();
+
+		const ONE_GWEI = 1_000_000_000;
+		await owner.sendTransaction({
+			to: faucet.address,
+			value: ONE_GWEI,
+			data: "0x" // data must be empty to invoke receive fallback
+		});
+	
+		return { faucet, MAX_WITHDRAWAL, MIN_WINDOW, owner, otherAccount };
+	}
+
+	describe("Deployment", function() {
+		it("Should set the correct state parameters", async function() {
+			const { faucet, MAX_WITHDRAWAL, MIN_WINDOW } = await loadFixture(deployContractFixture);
+
+			expect(await faucet.maxWithdrawal()).to.equal(MAX_WITHDRAWAL);
+			expect(await faucet.minWindow()).to.equal(MIN_WINDOW);
+		});
+	});
+
+	describe("Fallback", function() {
+		it("Should deposit the funds to the contract balance", async function() {
+			const { faucet, owner } = await loadFixture(deployContractFixture);
+			const ONE_GWEI = 1_000_000_000;
+			const tx = owner.sendTransaction({
+				to: faucet.address,
+				value: ONE_GWEI,
+				data: "0x" // data must be empty to invoke receive fallback
+			});
+			await expect(tx).to.changeEtherBalances(
+				[owner, faucet],
+				[-ONE_GWEI, ONE_GWEI]
+			);
+		});
+	});
+
+	describe("Withdrawals", function() {
+		describe("Validatons", function() {
+			it("Should revert with right error if no funds are available", async function() {
+				const { faucet, MAX_WITHDRAWAL } = await loadFixture(deployContractFixture);
+				await expect(faucet.withdraw(MAX_WITHDRAWAL)).to.be.revertedWith(
+					"Insufficient funds in faucet"
+				);
+			});
+
+			it("Should revert if the amount requested is larger than withdrawal limit", async function() {
+				const { faucet, MAX_WITHDRAWAL } = await loadFixture(deployContractWithFundsFixture);
+				await expect(faucet.withdraw(MAX_WITHDRAWAL*2)).to.be.revertedWith(
+					"Withdrawal limit exceeded"
+				);
+			});
+
+			it("Should revert if called too soon", async function() {
+				const { faucet, MAX_WITHDRAWAL } = await loadFixture(deployContractWithFundsFixture);
+				await faucet.withdraw(MAX_WITHDRAWAL);
+				await expect(faucet.withdraw(MAX_WITHDRAWAL)).to.be.revertedWithCustomError(
+					faucet,
+					"earlyWithdrawal"
+				);
+			});
+
+			it("Shouldn't fail if enough time has passed", async function() {
+				const { faucet, MAX_WITHDRAWAL, MIN_WINDOW } = await loadFixture(deployContractWithFundsFixture);
+				await faucet.withdraw(MAX_WITHDRAWAL);
+
+				await time.increase(MIN_WINDOW);
+
+				await expect(faucet.withdraw(MAX_WITHDRAWAL)).to.not.be.reverted;
+			});
+
+			it("Should handle multiple addresses", async function() {
+				const { faucet, otherAccount, MAX_WITHDRAWAL } = await loadFixture(deployContractWithFundsFixture);
+				await faucet.withdraw(MAX_WITHDRAWAL);
+				await expect(faucet.connect(otherAccount).withdraw(MAX_WITHDRAWAL)).not.to.be.reverted;
+			});
+		});
+
+		describe("Transfers", function() {
+			it("Should transfer the funds to the sender", async function() {
+				const { faucet, owner, MAX_WITHDRAWAL } = await loadFixture(deployContractWithFundsFixture);
+				await expect(faucet.withdraw(MAX_WITHDRAWAL)).to.changeEtherBalances(
+					[owner, faucet],
+					[MAX_WITHDRAWAL, -MAX_WITHDRAWAL]
+				);
+			});
+		});
+	});
+
+	describe("Setters", function() {
+		it("Should set a new withdrawal limit", async function() {
+			const { faucet, MAX_WITHDRAWAL } = await loadFixture(deployContractFixture);
+			expect(await faucet.maxWithdrawal()).to.equal(MAX_WITHDRAWAL);
+
+			await faucet.setMaxWithdrawal(2*MAX_WITHDRAWAL);
+			expect(await faucet.maxWithdrawal()).to.equal(2*MAX_WITHDRAWAL);
+		});
+
+		it("Should set a new time window", async function() {
+			const { faucet, MIN_WINDOW } = await loadFixture(deployContractFixture);
+			expect(await faucet.minWindow()).to.equal(MIN_WINDOW);
+
+			await faucet.setMinWindow(2*MIN_WINDOW);
+			expect(await faucet.minWindow()).to.equal(2*MIN_WINDOW);
+		});
+
+		it("Should fail to change limit if not owner", async function() {
+			const { faucet, otherAccount, MAX_WITHDRAWAL } = await loadFixture(deployContractFixture);
+			await expect(faucet.connect(otherAccount).setMaxWithdrawal(MAX_WITHDRAWAL*2))
+			.to.be.revertedWith("Not Owner");
+		});
+
+		it("Should fail to change window if not owner", async function() {
+			const { faucet, otherAccount, MIN_WINDOW } = await loadFixture(deployContractFixture);
+			await expect(faucet.connect(otherAccount).setMinWindow(MIN_WINDOW*2))
+			.to.be.revertedWith("Not Owner");
+		});
+	});
+});
